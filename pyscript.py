799601eb9100163a181e1f364eb814b7e10708c2
kanishk gahlot <kanishkgahlot.cse18@itbhu.ac.in>
2022-05-25 02:20:06 +0530

    added script file

---
+++ pyscript.py
@@ -0,0 +1,21 @@
+
+import os
+import random
+import time
+
+start_time = time.time()
+
+preurl = "curl -w \"\\n\" http://localhost:8080/shorten/https://"
+c = 5
+for i in range(c):
+  #len = random.randrange(6, 25)
+  len=13
+  keyString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+  midurl = ""
+  for i in range(len):
+    midurl = midurl + keyString[random.randrange(100,1000)%62];
+  url = preurl + midurl + ".com"
+  #print(url)
+  os.system(url)
+
+print("--- %s seconds ---" % (time.time() - start_time))
